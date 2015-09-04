5ae5f8371c4e00345c50167060ad1becbe8e317f
zyyecho <zyy_echo@163.com>
2015-09-04 17:06:52 +0800

    test

---
+++ src/test/java/com/smilehat/business/service/CategoryServiceTest.java
@@ -0,0 +1,14 @@
+package com.smilehat.business.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CategoryServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
